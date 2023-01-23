a89b1b97dc5284d782340ffddfa8160229d22866
FelixTM3911 <wolf2006tm@gmail.com>
2023-01-23 06:58:44 +0100

    few changes

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var triggerDiv = document.getElementById("readme");
-    var contentDiv = document.getElementById("bottomtext");
+// Ich hasse js
 
-    triggerDiv.onclick = function() {
-      if(contentDiv.style.display === "none") {
-        contentDiv.style.display = "block";
-      } else {
-        contentDiv.style.display = "none";
-      }
-    }
-    
+document.getElementById("readme").onclick = function() {
+  toggleDiv("bottomtext","readme");
+};
+
+function toggleDiv(id) {
+  var div = document.getElementById(id);
+  if (div.style.display === "none") {
+      div.classList.add("fade-in");
+      div.style.display = "block";
+      setTimeout(() => {
+          div.classList.remove("fade-in");
+      }, 500);
+  } else {
+      div.classList.add("fade-out");
+      setTimeout(() => {
+          div.style.display = "none";
+          div.classList.remove("fade-out");
+      }, 500);
+  }
+}
+
 document.getElementById('discord').addEventListener('click', changeText);
 
 function changeText(){
@@ -20,4 +31,21 @@ function changeText(){
     text.innerHTML = "Discord";
   }
 }
-});
+
+const typewriter = document.getElementById("copyright");
+
+const delay = 3750;
+const text = "© FelixTM - SfTb";
+let i = 0;
+
+setTimeout(function() {
+  function type() {
+  if (i < text.length) {
+      typewriter.innerHTML += text[i];
+      i++;
+      let speed = Math.floor(Math.random() * (500 - 200 + 1) + 200);
+      setTimeout(type, speed);
+  }
+  }
+  type();
+}, delay);
